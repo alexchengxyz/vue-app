edd714765001015b6c474f97952d37a064e5dd72
Bob Chen <Bobchen@Bobchen-Macbook.local>
2025-07-29 16:11:50 +0800

    init: 安裝 airbnb 等相關依賴
    1. 安裝相依套件
    2. 修正配置問題
    3.  移除 .prttierre 只保留 .prettierrc.json
    4. 新增 rules 規則 暫時解除 pug 引入元件但顯示無引入問題

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import { globalIgnores } from 'eslint/config';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
+import pluginVitest from '@vitest/eslint-plugin';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+	{
+		name: 'app/files-to-lint',
+		files: ['**/*.{ts,mts,tsx,vue}'],
+	},
+
+	globalIgnores([
+		'**/dist/**',
+		'**/dist-ssr/**',
+		'**/coverage/**',
+		'node_modules/',
+		'*.log',
+		'*.md',
+		'*.json',
+		'*.yaml',
+		'*.yml',
+		'.DS_Store',
+		'.vscode/',
+		'.idea/',
+		'*.min.*',
+		'public/',
+		'.husky/',
+		'*.local',
+	]),
+
+	pluginVue.configs['flat/essential'],
+	vueTsConfigs.recommended,
+
+	{
+		rules: {
+			'@typescript-eslint/no-unused-vars': 'off', // 暫時解決 pug 報錯問題
+		},
+	},
+
+	{
+		...pluginVitest.configs.recommended,
+		files: ['src/**/__tests__/*'],
+	},
+	skipFormatting
+);
