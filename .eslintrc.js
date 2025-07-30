26a892f4a533f014698c1409e0ee90afce928cb1
Bob Chen <Bobchen@Bobchen-Macbook.local>
2025-07-30 16:20:27 +0800

    init: 修正錯誤

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,47 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'airbnb-base',
-    'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier',
-    'plugin:prettier/recommended',
-  ],
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
-  },
-  plugins: ['vue', '@typescript-eslint'],
-  rules: {
-    'import/no-extraneous-dependencies': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-unresolved': 'off',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'vue/multi-word-component-names': 'off',
-    'vue/require-default-prop': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-param-reassign': [
-      'error',
-      {
-        props: true,
-        ignorePropertyModificationsFor: ['state', 'config', 'el', 'item', 'e'],
-      },
-    ],
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-      typescript: {},
-    },
-  },
+	root: true,
+	env: { browser: true, node: true, es2021: true },
+	extends: [
+		'eslint:recommended',
+		'airbnb-base',
+		'plugin:vue/vue3-recommended',
+		'plugin:vue-pug/vue3-recommended',
+		'@vue/eslint-config-typescript',
+		'@vue/eslint-config-prettier',
+		'plugin:prettier/recommended',
+	],
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		parser: '@typescript-eslint/parser',
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['vue', '@typescript-eslint', 'vue-pug'],
+	rules: {
+		'vue/script-setup-uses-vars': 'error',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'no-unused-vars': 'off',
+		'import/no-extraneous-dependencies': 'off',
+		'import/prefer-default-export': 'off',
+		'import/no-unresolved': 'off',
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{ js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
+		],
+		'vue/multi-word-component-names': 'off',
+		'vue/require-default-prop': 'off',
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-param-reassign': [
+			'error',
+			{ props: true, ignorePropertyModificationsFor: ['state', 'config', 'el', 'item', 'e'] },
+		],
+	},
+	settings: {
+		'import/resolver': {
+			node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+			typescript: {},
+		},
+	},
 };
